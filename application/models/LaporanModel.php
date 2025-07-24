6ca45978aedabb91384bf9f7e8a8ef32e720852d
ridwankimin <mochamadridwan008@gmail.com>
2025-07-24 11:04:51 +0700

    update laporan

--- application/models/LaporanModel.php
+++ application/models/LaporanModel.php
@@ -7,6 +7,8 @@ class LaporanModel extends CI_Model
         $this->db->select('user_presensi.id_user,user.nip, DAY(tanggal) AS day,user.bagian_id,user.upt_id,
         MIN(IF(jenis_presensi = "masuk", waktu, NULL)) AS waktu_presensi_masuk
         , MAX(IF(jenis_presensi = "pulang", waktu, NULL)) AS waktu_presensi_pulang
+        , MIN(IF(jenis_presensi = "masuk", (CASE WHEN set_waktu_presensi_id IS NULL OR set_waktu_presensi_id="1" OR set_waktu_presensi_id="2" THEN true END), NULL)) AS fwamasuk
+        , MAX(IF(jenis_presensi = "pulang", (CASE WHEN set_waktu_presensi_id IS NULL OR set_waktu_presensi_id="1" OR set_waktu_presensi_id="2" THEN true END), NULL)) AS fwapulang
         , MIN(IF(jenis_presensi = "masuk", batas_waktu_presensi, NULL)) AS batas_waktu_presensi_masuk
         , MAX(IF(jenis_presensi = "pulang", batas_waktu_presensi, NULL)) AS batas_waktu_presensi_pulang');
         $this->db->from('user_presensi');
@@ -15,7 +17,7 @@ class LaporanModel extends CI_Model
         $this->db->group_by('tanggal, user.id_user');
         $this->db->order_by('nip', 'asc');
         $getsub = $this->db->get_compiled_select();
-        $this->db->select('id_user,nip, day,bagian_id,
+        $this->db->select('id_user,nip, day,bagian_id,fwamasuk,fwapulang,
 					CASE
 						WHEN (waktu_presensi_masuk IS NULL OR waktu_presensi_masuk = "" OR waktu_presensi_masuk = "00:00:00") AND waktu_presensi_pulang > batas_waktu_presensi_pulang
 								THEN "tam"
@@ -32,8 +34,12 @@ class LaporanModel extends CI_Model
 						WHEN waktu_presensi_masuk > batas_waktu_presensi_masuk 
 								AND waktu_presensi_pulang < batas_waktu_presensi_pulang
 								THEN "tl_psw"
+						WHEN (waktu_presensi_masuk > batas_waktu_presensi_masuk AND (fwamasuk = true OR fwapulang = true) AND (waktu_presensi_pulang - batas_waktu_presensi_pulang) - (waktu_presensi_masuk - batas_waktu_presensi_masuk) > 0)
+								THEN "tl_f"
 						WHEN waktu_presensi_masuk > batas_waktu_presensi_masuk 
 								THEN "tl"
+						WHEN (waktu_presensi_pulang < batas_waktu_presensi_pulang AND (fwamasuk = true OR fwapulang = true) AND (waktu_presensi_pulang - batas_waktu_presensi_pulang) - (waktu_presensi_masuk - batas_waktu_presensi_masuk) > 0)
+								THEN "psw_f"
 						WHEN waktu_presensi_pulang < batas_waktu_presensi_pulang 
 								THEN "psw"
 						ELSE "tw"
@@ -64,6 +70,8 @@ class LaporanModel extends CI_Model
 							, MIN(IF(jenis_presensi = "masuk", batas_waktu_presensi, NULL)) AS batas_waktu_presensi_masuk
 							, MAX(IF(jenis_presensi = "pulang", batas_waktu_presensi, NULL)) AS batas_waktu_presensi_pulang
 							, MIN(IF(jenis_presensi = "masuk", cekwf, NULL)) AS jenis_absen_masuk
+							, MIN(IF(jenis_presensi = "masuk", (CASE WHEN set_waktu_presensi_id IS NULL OR set_waktu_presensi_id="1" OR set_waktu_presensi_id="2" THEN true END), NULL)) AS fwamasuk
+        					, MAX(IF(jenis_presensi = "pulang", (CASE WHEN set_waktu_presensi_id IS NULL OR set_waktu_presensi_id="1" OR set_waktu_presensi_id="2" THEN true END), NULL)) AS fwapulang
 							, MIN(IF(jenis_presensi = "pulang", cekwf, NULL)) AS jenis_absen_pulang');
         $this->db->from('user_presensi');
         $this->db->join('user', 'user.id_user=user_presensi.id_user');
@@ -101,8 +109,12 @@ class LaporanModel extends CI_Model
 							WHEN waktu_presensi_masuk > batas_waktu_presensi_masuk 
 									AND waktu_presensi_pulang < batas_waktu_presensi_pulang
 									THEN "Terlambat masuk dan pulang sebelum waktunya"
+							WHEN (waktu_presensi_masuk > batas_waktu_presensi_masuk AND (fwamasuk = true OR fwapulang = true) AND (waktu_presensi_pulang - batas_waktu_presensi_pulang) - (waktu_presensi_masuk - batas_waktu_presensi_masuk) > 0)
+									THEN "Terlambat masuk (FWA)"
 							WHEN waktu_presensi_masuk > batas_waktu_presensi_masuk 
 									THEN "Terlambat masuk"
+							WHEN (waktu_presensi_pulang < batas_waktu_presensi_pulang AND (fwamasuk = true OR fwapulang = true) AND (waktu_presensi_pulang - batas_waktu_presensi_pulang) - (waktu_presensi_masuk - batas_waktu_presensi_masuk) > 0)
+									THEN "Pulang sebelum waktunya (FWA)"
 							WHEN waktu_presensi_pulang < batas_waktu_presensi_pulang 
 									THEN "Pulang sebelum waktunya"
 							ELSE "Tepat waktu"
